0bf21b7b37278c9b160dbbe658b48d9a2a763c42
Chomiciak <chomiciak@gmail.com>
2019-04-04 16:45:18 +0200

    Customised Client creating form

--- resources/views/client/create.blade.php
+++ resources/views/client/create.blade.php
@@ -4,12 +4,45 @@
 
 <div class="container card">
   <div class="card-header">
-    Featured
+    Stwórz klienta
   </div>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <form>
+    <div class="form-group">
+        <label for="name">Imię</label>
+        <input type="text" class="form-control" id="name" placeholder="Podaj imię">
+    </div>
+    <div class="form-group">
+        <label for="surname">Nazwisko</label>
+        <input type="text" class="form-control" id="surname" placeholder="Podaj nazwisko">
+    </div>
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" placeholder="Podaj adres e-mail">
+    </div>
+    <div class="form-group">
+        <label for="phone">Telefon</label>
+        <input type="phone" class="form-control" id="phone"  placeholder="Podaj numer telefonu">
+    </div>    
+    <div class="form-group">
+        <label for="street">Ulica</label>
+        <input type="text" class="form-control" id="street" placeholder="Podaj ulicę">
+    </div>
+    <div class="form-group">
+        <label for="houseno">Numer domu</label>
+        <input type="text" class="form-control" id="houseno" placeholder="Podaj numer domu">
+    </div>
+    <div class="form-group">
+        <label for="city">Miasto</label>
+        <input type="text" class="form-control" id="city" placeholder="Podaj miasto">
+    </div>
+    <div class="form-group">
+        <label for="postalcode">Kod pocztowy</label>
+        <input type="text" class="form-control" id="postalcode" placeholder="Podaj kod pocztowy">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Dodaj klienta</button>
+    </form>
   </div>
 </div>
 
